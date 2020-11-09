4fa377a285245fb76e0372cf9bd4f66144412c77
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2020-11-09 08:54:33 +0100

    Add files via upload

---
+++ Varijable vježbe 1/Varijable vježbe 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varijable_vježbe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = -1000;
+            string c = "a";
+            float d = 3.123f;
+            double e = 3.123456789;
+            bool f = true;
+            string g = "\"true\"";
+            Console.Write("{0}, {1}, {2}, {3}, {4}, {5}, {6}",
+                a, b, c, d, e, f, g);
+            Console.ReadKey();
+        }
+    }
+}
